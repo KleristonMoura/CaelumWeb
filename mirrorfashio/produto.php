a910918c5856c8c151f3fa73ff4e44cc7f9c6894
Kleriston Moura <kleriston.moura@hotmail.com>
2019-09-17 00:17:38 -0300

    teste php

---
+++ mirrorfashio/produto.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Mirror Fashion</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+</head>
+
+<body>
+<?php include("cabecalho.php"); ?>
+<!-- Aqui vai vir o miolo da página depois! -->
+<?php include("rodape.php"); ?>
+
+</body>
+
+</html>
